8d035628243415fa3eb46aa6f8508982240d5816
Hachem Said <labsar.ahmed@outlook.fr>
2020-08-19 19:44:28 -0400

    added code to create workout model

--- models/Exercise.js
+++ models/Exercise.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 // to track the name, type, weight, sets, reps, and duration of exercise.
-const Exercise = new Schema({
+const ExerciseSchema = new Schema({
   name: String,
   type: String,
   weight: Number,
   sets: Number,
   reps: Number,
   duration: Number,
+  distance: Number,
 });
 
 const Exercice = mongoose.model("Exercise", ExerciseSchema);
